fb4ee04b08ee345b1691dd39664e1a9b118611da
Cordtus <Cordtus@gmail.com>
2024-05-07 05:07:18 -0600

    modified:   .eslintrc.json 	modified:   .gitignore 	modified:   eslint.config.mjs 	modified:   funcs/menuFuncs.js 	modified:   utils/repoUtils.js

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      ecmaVersion: 2021,
+      globals: {
+        ...globals.node, // Explicitly spread Node.js globals
+        process: "readonly",
+        require: "readonly",
+        module: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+      }
+    }
+  },
+  pluginJs.configs.recommended,
+];
